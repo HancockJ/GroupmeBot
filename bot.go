5488be99048602070322d771a21826fc493b9c90
BuildTools <jon.crosse@aol.com>
2020-10-02 23:30:31 -0400

    no you dont

--- bot.go
+++ bot.go
@@ -9,9 +9,5 @@ import (
 func main() {
 	fmt.Println(math.Sqrt(49)) // ;
 	fmt.Println("Hello Jack Hancock!")
-<<<<<<< HEAD
 	fmt.Println("Work please")
-=======
-	fmt.Println("I love beers!")
->>>>>>> 81eb8c032facbfcf238db1ab48574a42e6399811
 }
